536cf25d548ddbc1d277da609a8bfd82f08b0349
LIghtJUNction <106986785+LIghtJUNction@users.noreply.github.com>
2024-06-21 05:31:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r -o -X -ll BoxForRootAlpah-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'docs/*'
+zip -r -o -X -ll BoxForRootAlpha-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'CHANGELOG.md' -x 'update.json' -x 'build.sh' -x '.github/*' -x 'docs/*'
